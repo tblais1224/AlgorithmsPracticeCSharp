e1073e1bb4fb393df3cc89bce5823d560b865965
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-13 21:58:06 -0400

    sock merchant done

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,21 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AlgorithmsPractice
+class Solution
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // given a starting hour and number or hours passed display the following: 
-            //    the hour is now x:00 and y days have passed 
-            //   constraints =    starting hour: 1 <= x <= 12      hours passed: 1 <= y <= intMax   challenge: intMin <= y <= intMax
 
-            var timeMachine = new TimeMachine(3, 261);
-            Console.WriteLine(timeMachine.GetTime());
+    // Complete the sockMerchant function below.
+    public class Merchant
+    {
+        public int NumberOfSocks { get; set; }
+        public List<int> Socks { get; set; }
+        public int Pairs { get; set; }
+        public Merchant(int numberOfSocks, int[] socks)
+        {
+            if (numberOfSocks <= 100 && numberOfSocks >= 1)
+            {
+                NumberOfSocks = numberOfSocks;
+                Socks = new List<int>(socks);
+                Pairs = 0;
+            }
+            else
+            {
+                throw new InvalidOperationException("doesn't fit constraints");
+            }
         }
+
+        private void MatchSocks()
+        {
+            List<int> socksCopy = new List<int>();
+            foreach (var sock in Socks)
+            {
+                if (socksCopy.Contains(sock))
+                {
+                    Pairs++;
+                    socksCopy.Remove(sock);
+                }
+                else
+                {
+                    socksCopy.Add(sock);
+                }
+            }
+        }
+
+        public int GetPairs()
+        {
+            MatchSocks();
+            return Pairs;
+        }
+    }
+    static int SockMerchant(int n, int[] ar)
+    {
+        var merchant = new Merchant(n, ar);
+        return merchant.GetPairs();
+    }
+
+    static void Main(string[] args)
+    {
+
+        int n = 9;
+        int[] ar = { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+
+        int result = SockMerchant(n, ar);
+        Console.WriteLine(result);
     }
 }
