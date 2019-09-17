ca46448fc363a665f2257fab2101ae2c628d1999
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-16 21:41:15 -0400

    refactored fibonacci number

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -6,38 +6,10 @@ namespace AlgorithmsPractice
 {
     static class Program
     {
-        public class SumOddFibonacciNumbers
-        {
-            public int FibsLessThanMe { get; set; }
-
-            private readonly List<int> _fibonacciNumbers = new List<int> { 0, 1 };
-
-            public SumOddFibonacciNumbers(int fibsLessThanMe)
-            {
-                FibsLessThanMe = fibsLessThanMe;
-            }
-
-            private void GetFibonacciNumbers()
-            {
-                while (true)
-                {
-                    var fibonacciNumber = _fibonacciNumbers[_fibonacciNumbers.Count - 1] + _fibonacciNumbers[_fibonacciNumbers.Count - 2];
-                    _fibonacciNumbers.Add(fibonacciNumber);
-                    if (fibonacciNumber + _fibonacciNumbers[_fibonacciNumbers.Count - 2] <= FibsLessThanMe) continue;
-                    break;
-                }
-            }
-
-            public int SumOddFibonacciNumber()
-            {
-                GetFibonacciNumbers();
-                return _fibonacciNumbers.Where(number => number % 2 != 0).Sum();
-            }
-        }
-
         private static void Main(string[] args)
         {
-            var sumFibs = new SumOddFibonacciNumbers(4000000);
+            var sumFibs = new FibonacciNumbers(10);
+            sumFibs.GetFibonacciNumbers();
             Console.WriteLine(sumFibs.SumOddFibonacciNumber());
         }
     }
