b2660208d1a2a3531c3f730d4f4f6f8bb9266fc5
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-16 22:25:03 -0400

    factorialize is done

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AlgorithmsPractice
 {
     static class Program
     {
+        public static int GetFactorialize(int numberToFactorialize)
+        {
+            if (numberToFactorialize == 0) return 1;
+            return numberToFactorialize * GetFactorialize(numberToFactorialize - 1);
+        }
         private static void Main(string[] args)
         {
-            var sumFibs = new FibonacciNumbers(10);
-            sumFibs.GetFibonacciNumbers();
-            Console.WriteLine(sumFibs.SumOddFibonacciNumber());
+            Console.WriteLine(Program.GetFactorialize(5));
         }
     }
 }
