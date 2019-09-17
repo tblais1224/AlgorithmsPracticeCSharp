73dae6a3cd9b2d5e63b3809dceb2fe990b5d7082
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-16 23:03:48 -0400

    sum all primes is refactored and completed

---
+++ AlgorithmsPractice/PrimeNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsPractice
+{
+    public class PrimeNumbers
+    {
+        public int MaxPrimeNumber { get; set; }
+        private readonly List<int> _primeNumbers;
+
+        public PrimeNumbers(int maxPrimeNumber)
+        {
+            if (maxPrimeNumber < 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxPrimeNumber));
+            }
+            MaxPrimeNumber = maxPrimeNumber;
+            _primeNumbers = new List<int>() { 2 };
+        }
+
+        public List<int> GetPrimes()
+        {
+            for (var i = 3; i <= MaxPrimeNumber; i += 2)
+            {
+                var primeCheck = _primeNumbers.All(prime => i % prime != 0);
+                if (primeCheck) _primeNumbers.Add(i);
+            }
+            return _primeNumbers;
+        }
+
+        public int SumPrimes()
+        {
+            return _primeNumbers.Sum();
+        }
+
+    }
+}
--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,20 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 namespace AlgorithmsPractice
 {
     static class Program
     {
-        public static int GetFactorialize(int numberToFactorialize)
-        {
-            if (numberToFactorialize == 0) return 1;
-            return numberToFactorialize * GetFactorialize(numberToFactorialize - 1);
-        }
         private static void Main(string[] args)
         {
-            Console.WriteLine(Program.GetFactorialize(5));
+            var primes = new PrimeNumbers(977);
+            primes.GetPrimes();
+            Console.WriteLine(primes.SumPrimes());
         }
     }
 }
