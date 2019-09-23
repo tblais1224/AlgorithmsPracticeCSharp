6119ab8a70f7fdd330e156f55ea41dd73e5f4485
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-23 11:00:08 -0400

    sum all numbers in a range

---
+++ AlgorithmTests/SumAllInRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmsPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace AlgorithmTests
+{
+    [TestFixture]
+    public class SumAllInRange
+    {
+        [TestCase(10, new int[] { 1, 4 })]
+        [TestCase(10, new int[] { 4, 1 })]
+        [TestCase(45, new int[] { 5, 10 })]
+        [TestCase(45, new int[] { 10, 5 })]
+        public void TestMethod1(int expected, int[] value)
+        {
+            var testMe = new SumAll(value);
+            Assert.AreEqual(expected, testMe.SumArray());
+        }
+    }
+}
--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -5,67 +5,28 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace AlgorithmsPractice
 {
-
-    public class MigratoryBirds
+    public class SumAll
     {
-        public List<int> Birds { get; set; }
-        private Dictionary<int, int> _birds = new Dictionary<int, int>();
-        private int _mostFrequentBird;
-        public MigratoryBirds(List<int> birds)
+        public int[] ArrayNumbers { get; set; }
+
+        public SumAll(int[] arraysNumbers)
         {
-            Birds = birds;
-            _mostFrequentBird = birds[0];
+            ArrayNumbers = arraysNumbers;
         }
 
-        public int MostFrequentBird()
+        public int SumArray()
         {
-            BuildBirdDictionary();
-            foreach (var bird in _birds.Where(bird => bird.Value >= _birds[_mostFrequentBird]))
-            {
-                if (bird.Key >= _mostFrequentBird && bird.Value == _birds[_mostFrequentBird]) continue;
-                _mostFrequentBird = bird.Key;
-            }
+            var result = 0;
+            for (int i = ArrayNumbers.Min(); i <= ArrayNumbers.Max(); i++) result += i;
 
-            return _mostFrequentBird;
-        }
-
-        private void BuildBirdDictionary()
-        {
-            foreach (var id in Birds)
-            {
-                if (_birds.ContainsKey(id))
-                    AddToExistingBird(id);
-                else
-                    AddNewBirdToDictionary(id);
-            }
-        }
-
-        private void AddNewBirdToDictionary(int id)
-        {
-            _birds.Add(id, 1);
-        }
-
-        private void AddToExistingBird(int id)
-        {
-            _birds[id]++;
+            return result;
         }
     }
     class Solution
     {
-        static int migratoryBirds(List<int> arr)
-        {
-            var birds = new MigratoryBirds(arr);
-            return birds.MostFrequentBird();
-        }
-
         static void Main(string[] args)
         {
-            List<int> arr = new List<int>() { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4 };
 
-            int result = migratoryBirds(arr);
-
-            Console.WriteLine(result);
         }
-
     }
 }
