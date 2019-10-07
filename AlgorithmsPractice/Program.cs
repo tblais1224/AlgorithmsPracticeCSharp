8132d87663fccf85263a80294588436c0e5efc68
tblais1224 <thomasblais.student@careerdevs.com>
2019-10-06 20:54:15 -0400

    woking on new year chaos

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,14 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmsPractice
 {
+    public class NewYearChaos
+    {
+        private List<int> _startQueue = new List<int>();
+        public List<int> QueueFinalState { get; set; }
+
+        public NewYearChaos(int[] queueFinalState)
+        {
+            QueueFinalState = queueFinalState.ToList();
+
+        }
+
+        public void Bribes()
+        {
+            MakeStartQueue();
+
+            int bribes = 0;
+
+            for (int i = 0; i < QueueFinalState.Count; i++)
+            {
+                if (QueueFinalState[i] == _startQueue[i]) continue;
+                bribes = Bribe(i, bribes);
+            }
+
+            if (QueueFinalState == _startQueue)
+                Console.WriteLine("{0}", bribes);
+            else
+                Console.WriteLine("Too chaotic");
+        }
+
+        private int Bribe(int i, int bribes)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                if (QueueFinalState[i] == _startQueue[i]) break;
+                bribes++;
+                SwapInts(QueueFinalState[i] - j);
+            }
+
+            return bribes;
+        }
+
+        private void MakeStartQueue()
+        {
+            for (int i = 1; i <= QueueFinalState.Count; i++)
+                _startQueue.Add(i);
+        }
+
+        private void SwapInts(int ValueBribing)
+        {
+            int temp = _startQueue[ValueBribing - 2];
+            _startQueue[ValueBribing - 2] = _startQueue[ValueBribing - 1];
+            _startQueue[ValueBribing - 1] = temp;
+        }
+
+    }
     class Solution
     {
 
         // Complete the minimumBribes function below.
         static void minimumBribes(int[] q)
         {
-
+            var bribes = new NewYearChaos(q);
+            bribes.Bribes();
 
         }
 
