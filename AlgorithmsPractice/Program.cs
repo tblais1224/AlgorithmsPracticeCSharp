f3405e166b580d84655399fc759624e82658b583
tblais1224 <thomasblais.student@careerdevs.com>
2019-10-08 09:09:42 -0400

    min swap 2 is complete

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,58 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 
 namespace AlgorithmsPractice
 {
-    static class Solution
+    public class SwapArrays
     {
+        public int[] UserArray { get; set; }
 
-        // Complete the minimumBribes function below.
-        static void minimumBribes(int[] q)
+        private int _swaps = 0;
+
+        public SwapArrays(int[] userArray)
         {
-            int bribes = 0;
-            bool check = true;
-            int minNum = q.Length;
-            for (int i = q.Length - 1; i >= 0; i--)
-            {
-                if (q[i] - i > 3)
-                {
-                    Console.WriteLine("Too chaotic");
-                    check = false;
-                    break;
-                }
+            UserArray = userArray;
+        }
 
-                if (q[i] - 3 == i)
+        public int OrderArray()
+        {
+            for (int i = UserArray.Length -1; i >= 0; i--)
+            {
+                while (UserArray[i] != i + 1)
                 {
-                    bribes += 2;
-                }
-                else
-                {
-                    if (minNum > q[i])
-                        minNum = q[i];
-                    else if (q[i] != minNum)
-                    {
-                        bribes++;
-                    }
+                    Sort(i);
                 }
             }
 
-            if (check) Console.WriteLine("{0}", bribes);
+            return _swaps;
         }
 
+        private void Sort(int index)
+        {
+            var temp = UserArray[UserArray[index] - 1];
+            UserArray[UserArray[index] - 1] = UserArray[index];
+            UserArray[index] = temp;
+            _swaps++;
+        }
+    }
+    class Solution
+    {
+
+        // Complete the minimumSwaps function below.
+        static int minimumSwaps(int[] arr)
+        {
+            var orderArray = new SwapArrays(arr);
+            return orderArray.OrderArray();
+
+        }
 
         static void Main(string[] args)
         {
-            int t = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            for (int tItr = 0; tItr < t; tItr++)
-            {
-                int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+                ;
+            int res = minimumSwaps(arr);
 
-                int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp))
-                    ;
-                minimumBribes(q);
-            }
+            Console.WriteLine(res);
+
         }
     }
 }
