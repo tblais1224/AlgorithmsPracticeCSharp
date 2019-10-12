ac56d49c99f6c8793bada64541b1f11fd0355215
tblais1224 <thomasblais.student@careerdevs.com>
2019-10-11 22:24:14 -0400

    two string done

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,69 +1,41 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Security.Policy;
+using System.Linq;
 
 namespace AlgorithmsPractice
 {
-
-    public class RansomNote
-    {
-        public Dictionary<string, int> Magazine { get; set; }
-
-        public string[] Note { get; set; }
-
-        public RansomNote(string[] note)
-        {
-            Note = note;
-        }
-
-        private void BuildDictionary(string[] magazine)
-        {
-            Magazine = new Dictionary<string, int>();
-            foreach (var word in magazine)
-            {
-                if (!Magazine.ContainsKey(word)) Magazine.Add(word, 1);
-                else Magazine[word]++;
-            }
-        }
-
-        public string CanMakeRansomNote(string[] magazine)
-        {
-            BuildDictionary(magazine);
-
-            foreach (var word in Note)
-            {
-                if (!Magazine.ContainsKey(word) || Magazine[word] == 0) return "No";
-                else Magazine[word]--;
-            }
-
-            return "Yes";
-        }
-    }
     class Solution
     {
 
-        // Complete the checkMagazine function below.
-        static void checkMagazine(string[] magazine, string[] note)
+        // Complete the twoStrings function below.
+        static string twoStrings(string s1, string s2)
         {
-            var ransom = new RansomNote(note);
-            Console.WriteLine(ransom.CanMakeRansomNote(magazine));
+            return s2.Any(MakeDictionary(s1).ContainsValue) ? "YES" : "NO";
+        }
+        static Dictionary<int, char> MakeDictionary(string s1)
+        {
+            var s1Dict = new Dictionary<int, char>();
+            foreach (var letter in s1.Where(letter => !s1Dict.ContainsValue(letter)))
+                s1Dict.Add(s1.IndexOf(letter), letter);
 
+            return s1Dict;
         }
 
         static void Main(string[] args)
         {
-            string[] mn = Console.ReadLine().Split(' ');
 
-            int m = Convert.ToInt32(mn[0]);
+            int q = Convert.ToInt32(Console.ReadLine());
 
-            int n = Convert.ToInt32(mn[1]);
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s1 = Console.ReadLine();
 
-            string[] magazine = Console.ReadLine().Split(' ');
+                string s2 = Console.ReadLine();
 
-            string[] note = Console.ReadLine().Split(' ');
+                string result = twoStrings(s1, s2);
 
-            checkMagazine(magazine, note);
+                Console.WriteLine(result);
+            }
         }
     }
 }
