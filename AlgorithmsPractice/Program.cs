c573821099ffeef98c799c28a0c66d3c3b2d2798
tblais1224 <thomasblais.student@careerdevs.com>
2019-10-21 13:50:26 -0400

    bubble sort done

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,32 +1,80 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
 
-class Solution
+namespace AlgorithmsPractice
 {
-
-    // Complete the catAndMouse function below.
-    static string catAndMouse(int x, int y, int z)
+    public class BubbleSort
     {
-        return Math.Abs(z - x) == Math.Abs(z - y) ? "Mouse C" : Math.Abs(z - x) > Math.Abs(z - y) ? "Cat B" : "Cat A";
-    }
+        public int[] Arr { get; set; }
+        public int Swaps { get; set; }
 
-    static void Main(string[] args)
-    {
-
-        int q = Convert.ToInt32(Console.ReadLine());
-
-        for (int qItr = 0; qItr < q; qItr++)
+        public BubbleSort(int[] arr)
         {
-            string[] xyz = Console.ReadLine().Split(' ');
+            Arr = arr;
+            Swaps = 0;
+        }
 
-            int x = Convert.ToInt32(xyz[0]);
+        public int[] SortArray()
+        {
+            if (Swaps != 0) Swaps = 0;
 
-            int y = Convert.ToInt32(xyz[1]);
+            foreach (var element in Arr)
+            {
+                for (int j = 0; j < Arr.Length - 1; j++)
+                {
+                    // Swap adjacent elements if they are in decreasing order
+                    if (Arr[j] > Arr[j + 1])
+                    {
+                        swap(j);
+                    }
+                }
+            }
 
-            int z = Convert.ToInt32(xyz[2]);
+            return Arr;
+        }
 
-            string result = catAndMouse(x, y, z);
-
-            Console.WriteLine(result);
+        private void swap(int index)
+        {
+            Swaps++;
+            int original = Arr[index];
+            Arr[index] = Arr[index + 1];
+            Arr[index + 1] = original;
         }
     }
+
+    static class Solution
+    {
+
+        // Complete the countSwaps function below.
+        static void countSwaps(int[] a)
+        {
+            var sort = new BubbleSort(a);
+            sort.SortArray();
+            Console.WriteLine("Array is sorted in {0} swaps.", sort.Swaps);
+            Console.WriteLine("First Element: {0}", sort.Arr[0]);
+            Console.WriteLine("Last Element: {0}", sort.Arr[sort.Arr.Length - 1]);
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+                ;
+            countSwaps(a);
+        }
+    }
+
 }
