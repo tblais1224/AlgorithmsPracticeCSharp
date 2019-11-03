93065ffd1cb3e5d6a2ab79604ae359c88cd64cf7
tblais1224 <thomasblais.student@careerdevs.com>
2019-11-03 16:54:55 -0500

    finished tale of twp stacks

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,20 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
-class Solution
+namespace AlgorithmsPractice
 {
-    public static int Fibonacci(int n)
+    public class Queue
     {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
-    }
+        private List<int> _queue = new List<int>();
 
-    static void Main(String[] args)
+        public void Enqueue(int num)
+        {
+            _queue.Add(num);
+        }
+
+        public int Dequeue()
+        {
+            int output = _queue[0];
+            _queue.Remove(output);
+            return output;
+        }
+
+        public void PrintFront()
+        {
+            Console.WriteLine(_queue[0]);
+        }
+    }
+    class Solution
     {
-        int n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(Fibonacci(n));
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var queue = new Queue();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+                if (Convert.ToInt32(input[0]) == 1)
+                {
+                    queue.Enqueue(Convert.ToInt32(input[1]));
+                }
+                else if (Convert.ToInt32(input[0]) == 2)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.PrintFront();
+                }
+            }
+        }
     }
 }
